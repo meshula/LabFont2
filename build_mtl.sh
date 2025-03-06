d50cd211e7b82a49af529225990576aaa57c2508
Nick Porcino <meshula@hotmail.com>
2025-03-06 09:46:27 -0800

    Remove hardcoded build scripts that will be generated by configure.py

--- build_mtl.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build && cd build
-cmake -G Xcode -DLABFONT_ENABLE_METAL=ON -DLABFONT_ENABLE_VULKAN=OFF -DLABFONT_ENABLE_WGPU=OFF ..
-make
-./tests/labfont_tests
-cd ..
--- build_vk.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p build && cd build
-VULKAN_SDK=/Users/nporcino/bin/VulkanSDK/1.4.304.1/macOS cmake -DLABFONT_ENABLE_VULKAN=ON -DLABFONT_ENABLE_WGPU=OFF ..
-make
-./tests/labfont_tests
-cd ..
-
